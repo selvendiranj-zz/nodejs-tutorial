4617595773dd0930d2fbbdb031316129a8f49191
selvendiranj <selvendiranj@gmail.com>
2017-08-28 17:02:29 +0530

    formatting changes

--- Samples/passport-authentication/config/index.js
+++ Samples/passport-authentication/config/index.js
@@ -1,8 +1,8 @@
 const config = {}
 
 config.redisStore = {
-  url: process.env.REDIS_STORE_URI,
-  secret: process.env.REDIS_STORE_SECRET
+    url: process.env.REDIS_STORE_URI,
+    secret: process.env.REDIS_STORE_SECRET
 }
 
 module.exports = config
--- Samples/passport-authentication/index.js
+++ Samples/passport-authentication/index.js
@@ -1,10 +1,10 @@
 const app = require('./app')
 const port = process.env.PORT || 3000
 
-app.listen(port, function (err) {
-  if (err) {
-    throw err
-  }
+app.listen(port, function(err) {
+    if (err) {
+        throw err
+    }
 
-  console.log(`server is listening on ${port}...`)
+    console.log(`server is listening on ${port}...`)
 })
